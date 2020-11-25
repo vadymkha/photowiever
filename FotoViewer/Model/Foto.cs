05bdca8ab4b29f2ee2d9cb1a8308f37b9851c39e
Vadimkha <sarvyksoftdev@gmail.com>
2020-11-25 18:18:58 +0200

    Добавьте файлы проекта.

---
+++ FotoViewer/Model/Foto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FotoViewer.Helpers;
+
+namespace FotoViewer.Model
+{
+    public class Foto: ObservableObject
+    {
+        private string filename;
+
+        public string Filename
+        {
+            get { return filename; }
+            set {
+                filename = value;
+                OnPropertyChanged("Filename");
+            }
+        }
+
+        private string filepath;
+
+        public string Filepath
+        {
+            get { return filepath; }
+            set
+            {
+                filepath = value;
+                OnPropertyChanged("Filepath");
+            }
+        }
+
+        private int filesize;
+
+        public int Filesize
+        {
+            get { return filesize; }
+            set
+            {
+                filesize = value;
+                OnPropertyChanged("Filesize");
+            }
+        }
+
+
+
+    }
+}
---
+++ FotoViewer/ViewModel/MainWindowVM.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FotoViewer.Helpers;
+using FotoViewer.Model;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+using System.IO;
+
+namespace FotoViewer.ViewModel
+{
+    public class MainWindowVM: ObservableObject
+    {
+        public ObservableCollection<Foto> Album { get; set; }
+
+        private Foto selectedFoto;
+        public Foto SelectedFoto
+        {
+            get { return selectedFoto; }
+            set
+            {
+                selectedFoto = value;
+                OnPropertyChanged("SelectedFoto");
+            }
+        }
+
+        public MainWindowVM()
+        {
+            Album = new ObservableCollection<Foto>();
+        }
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ?? (addCommand = new RelayCommand(
+                    obj =>
+                    {
+                        OpenFileDialog openDialog = new OpenFileDialog();
+                        openDialog.Filter = "JPEG files (*.JPEG)|*.JPEG|All files (*.*)|*.*||";
+                        if(openDialog.ShowDialog() == true)
+                        {
+                            FileInfo file = new FileInfo(openDialog.FileName);
+                            Foto foto = new Foto { Filepath = openDialog.FileName, Filename = file.Name, Filesize = (int)file.Length };
+                            Album.Add(foto);
+                            SelectedFoto = foto;
+                        }
+                    }
+                    ));
+            }
+        }
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ?? (removeCommand = new RelayCommand(
+                    obj =>
+                    {
+                        Foto foto = obj as Foto;
+                        Album.Remove(foto);
+                    },
+                    (obj)=>
+                    {
+                        return Album.Count > 0;
+                    }
+                    ));
+            }
+        }
+    }
+}
