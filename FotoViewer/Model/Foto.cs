a41a3007c5e106874f1678b62bcb565e6b4b2b5a
Vadimkha <sarvyksoftdev@gmail.com>
2020-11-25 18:39:10 +0200

    add property description

--- FotoViewer/Model/Foto.cs
+++ FotoViewer/Model/Foto.cs
@@ -19,7 +19,16 @@ namespace FotoViewer.Model
                 OnPropertyChanged("Filename");
             }
         }
-
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
         private string filepath;
 
         public string Filepath
